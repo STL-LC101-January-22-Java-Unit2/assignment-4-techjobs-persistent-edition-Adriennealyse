3b84681f3da5840e0bac166944c95ce6001e355b
Adrienne Davis <adrienne@adriennealyse.com>
2022-06-28 15:00:02 -0500

    Part 4 SQL Query.

--- queries.sql
+++ queries.sql
@@ -8,7 +8,4 @@
 
 -- Part 3: drop table job;
 
--- Part 4: select * from skill
---           left join job_skills on skill.id = job_skills.skills_id
---           where job_skills.jobs_id is not null
---           order by name asc;
+-- Part 4: select * from skill left join job_skills on skill.id = job_skills.skills_id where job_skills.jobs_id is not null order by name asc;
