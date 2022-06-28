2ff384f80d7044499403bbd0cc290c949f236589
Adrienne Davis <adrienne@adriennealyse.com>
2022-06-28 14:54:33 -0500

    Sql query part 4.

--- queries.sql
+++ queries.sql
@@ -9,6 +9,6 @@
 -- Part 3: drop table job;
 
 -- Part 4: select * from skill
---           inner join job_skills on job_skills.skills_id = skill.id
+--           left join job_skills on skill.id = job_skills.skills_id
 --           where job_skills.jobs_id is not null
 --           order by name asc;
